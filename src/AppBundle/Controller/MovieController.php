780d093bba5ee2f72c908d1fc56b63648fdd6194
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-18 23:20:09 +0200

    Update form (live templates play in PhpStorm)

--- src/AppBundle/Controller/MovieController.php
+++ src/AppBundle/Controller/MovieController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Movie;
 use AppBundle\Form\MovieType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class MovieController extends Controller
@@ -13,11 +14,18 @@ class MovieController extends Controller
     /**
      * @Route("/movies/new", name="movies_new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
         $movie = new Movie();
 
         $form = $this->createForm(new MovieType(), $movie);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            // todo - do saving
+
+            $this->addFlash('success', 'Sam would be proud');
+            $this->redirectToRoute('movies_list', array());
+        }
 
         return $this->render('movie/new.html.twig', [
             'quote' => 'If my answers frighten you then you should cease asking scary questions. (Pulp Fiction)',
